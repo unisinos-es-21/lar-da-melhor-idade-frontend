880585f10c44a86b7fd8f36f74e8585ab72fe571
Vbobell <vinicius.bobel41@gmail.com>
2021-09-08 17:52:31 -0300

    feat: create react structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Color, Size, Title } from "@lar_melhor_idade/design-system";
+
+function App() {
+  return (
+    <div>
+      <Title color={Color.BLACK} size={Size.MEDIUM} type="h2">
+        Hello world
+      </Title>
+    </div>
+  );
+}
+
+export default App;
