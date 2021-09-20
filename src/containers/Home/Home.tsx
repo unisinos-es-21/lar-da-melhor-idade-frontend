7c3c06ce65adfc28eca71d750a7fc4940ba7013f
Vbobell <vinicius.bobel41@gmail.com>
2021-09-20 13:34:27 -0300

    feat(home): create home page

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,11 +1,43 @@
 import { RouteComponentProps } from '@reach/router';
 
-import { useHello } from '../../api/hello';
+import {
+  Button,
+  Card,
+  Header,
+  Icon,
+  Title,
+  Color,
+} from '@lar_melhor_idade/design-system';
 
 export interface HomeProps extends RouteComponentProps {}
 
 export function Home(props: HomeProps) {
-  const data = useHello();
-
-  return <div>{data ? data : 'aguarde..'}</div>;
+  return (
+    <section
+      className="flex flex-col justify-start items-center w-full min-h-screen bg-white space-y-8 md:p-0"
+      {...props}
+    >
+      <Header className="px-4 md:px-0">
+        <Title type="h1">ILPI Melhor Idade</Title>
+      </Header>
+      <div className="container grid grid-cols-1 px-4 space-y-8 md:grid-cols-4 md:px-0">
+        <Card>
+          <Card.Header className="flex items-center space-x-4">
+            <Icon icon="user" size="2x" />
+            <Title color={Color.WHITE}>Institucionalizados</Title>
+          </Card.Header>
+          <Card.Content>
+            <Button
+              className="w-full"
+              type="button"
+              color={Color.BLACK}
+              icon="arrow-right"
+            >
+              Acessar
+            </Button>
+          </Card.Content>
+        </Card>
+      </div>
+    </section>
+  );
 }
