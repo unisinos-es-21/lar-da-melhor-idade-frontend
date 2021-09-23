0e0221490a26facf6a500563c9647d2caedd007b
ilegra.leite <ilegra.leite@hypeflame.com.br>
2021-09-23 18:35:41 -0300

    feat(institutionalized): create view institutionalized list

--- src/api/institutionalized.ts
+++ src/api/institutionalized.ts
@@ -1,10 +1,28 @@
 import { client } from './client';
 
-import { InstitutionalizedRequest } from './interface/institutionalized';
+import {
+  InstitutionalizedRequest,
+  InstitutionalizedListRequest,
+  InstitutionalizedListResponse,
+} from './interface/institutionalized';
 
 export function register(institutionalizedRequest: InstitutionalizedRequest) {
   return client.post<InstitutionalizedRequest>(
     'institutionalized',
     institutionalizedRequest
   );
+}
+
+export function getList(
+  param: InstitutionalizedListRequest = { pageNumber: 0, cpf: '', name: '' }
+) {
+  const params = new URLSearchParams();
+
+  params.append('pageNumber', String(param.pageNumber ?? 0));
+  params.append('cpf', param.cpf ?? '');
+  params.append('name', param.name ?? '');
+
+  const url = `institutionalized?${params.toString()}`;
+
+  return client.get<InstitutionalizedListResponse>(url);
 }
